44d2a6688c6e2ccf8f6548472c5e9dde80b54645
Bryllith <bryllith.armijo7233@utc.edu.ec>
2024-10-30 09:46:27 -0500

    raiz
    calcular  la raiz

---
+++ funcion.py
@@ -0,0 +1,20 @@
+def sumar(n1,n2):
+    return n1+n2
+
+def restar(n1,n2):
+    return n1-n2
+
+def multiplicar(n1,n2):
+    return n1*n2
+
+def dividir(n1,n2):
+    return n1/n2
+
+n1 = int (input("ingresa el numero 1:"))
+n2 = int (input("ingresa el numero 2:"))
+
+print("La suma es: ", sumar(n1,n2))
+print("La resta es: ", restar(n1,n2))
+print("La multiplicación es: ", multiplicar(n1,n2))
+print("La dividir es: ", dividir(n1,n2))
+
--- restar.py
+++ restar.py
@@ -1,4 +1,4 @@
 x=4
 y=5
 z=x-y
-print("la resta es;" , 7)
+print("la resta es;" , z)
